98bccadddbe60c530929fb3bedde8a9967f98108
hawkup <spiritsquare@gmail.com>
2023-02-15 05:28:37 +0700

    type-challenges: solve 16-pop

--- type-challenges/medium/16-pop.ts
+++ type-challenges/medium/16-pop.ts
@@ -26,7 +26,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Pop<T extends any[]> = any
+type Pop<T extends unknown[]> =
+  T extends [...infer Head, unknown]
+  ? Head
+  : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
