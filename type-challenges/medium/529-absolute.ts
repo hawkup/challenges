7e64f4e3aca45eddfb6045f114eed0aa00d27d45
hawkup <spiritsquare@gmail.com>
2023-02-15 19:31:11 +0700

    type-challenges: solve 529-absolute

--- type-challenges/medium/529-absolute.ts
+++ type-challenges/medium/529-absolute.ts
@@ -19,7 +19,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Absolute<T extends number | string | bigint> = any
+type Absolute<T extends number | string | bigint> =
+  `${T}` extends `${infer Head}${infer Tail}`
+  ? Head extends '-'
+    ? Tail
+    : `${T}`
+  : `${T}`
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
