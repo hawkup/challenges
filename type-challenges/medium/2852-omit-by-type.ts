d5538af675c25d7e9b2a6a9c8aaec1ca0808cff4
hawkup <spiritsquare@gmail.com>
2023-02-19 20:52:34 +0700

    type-challenges: solve 2852-omit-by-type

--- type-challenges/medium/2852-omit-by-type.ts
+++ type-challenges/medium/2852-omit-by-type.ts
@@ -23,7 +23,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type OmitByType<T, U> = any
+type OmitByType<T, U> = {
+  [P in keyof T as T[P] extends U ? never : P]: T[P]
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
